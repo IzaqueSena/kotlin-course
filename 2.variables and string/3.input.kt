122359b2aef16a6f71ff9fa1bf533127a25665d7
Izaque Sena <izaqueseninha@gmail.com>
2024-01-05 22:06:06 -0300

    inpit

--- 2.variables and string/3.input.kt
+++ 2.variables and string/3.input.kt
@@ -1,6 +1,13 @@
 fun main() {
     // # input
     println("What's your name?")
-    val userInput = readLine()
+    var userInput = readLine()?:"" // ?: is the elvis operatoor, if readLine() is null, then set it to "" 
     println("You wrote: $userInput")
+
+    // input is read as a String
+    // but can be converted to other types
+    println("What's your age?")
+    var age = readLine()?.toIntOrNull()?:0
+    println("You are ${age} years old")
+
 }
